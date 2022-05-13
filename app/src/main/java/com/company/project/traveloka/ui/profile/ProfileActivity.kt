8250735f64fac0b935cf129fb82b232309e19692
lvinist <alvin.geomatics@gmail.com>
2022-05-13 19:36:29 +0700

    Create profile activity

--- app/src/main/java/com/company/project/traveloka/ui/profile/ProfileActivity.kt
+++ app/src/main/java/com/company/project/traveloka/ui/profile/ProfileActivity.kt
@@ -1,12 +1,28 @@
 package com.company.project.traveloka.ui.profile
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
+import com.company.project.traveloka.R
 import com.company.project.traveloka.databinding.ActivityProfileBinding
+import com.company.project.traveloka.ui.profile.adapter.ProfilesPagerAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ProfileActivity : AppCompatActivity() {
+
+    companion object {
+        @StringRes
+        private val TAB_TITLES = intArrayOf(
+            R.string.tab_text_post,
+            R.string.tab_text_review,
+            R.string.tab_text_koleksi
+        )
+    }
 
     private lateinit var binding: ActivityProfileBinding
 
@@ -14,5 +30,33 @@ class ProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        //val profilesPagerAdapter = ProfilesPagerAdapter(this)
+        binding.apply {
+            //viewpagerProfile.adapter = profilesPagerAdapter
+           // TabLayoutMediator(tabProfile, viewpagerProfile) { tab, position ->
+           //     tab.text = resources.getString(TAB_TITLES[position])
+           // }.attach()
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.share -> {
+                Toast.makeText(this, "Fitur belum dibuat", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.help -> {
+                Toast.makeText(this, "Fitur belum dibuat", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.home_menu -> {
+                Toast.makeText(this, "Fitur belum dibuat", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            else -> return true
+        }
     }
 }
---
+++ app/src/main/java/com/company/project/traveloka/ui/profile/adapter/ProfilesPagerAdapter.kt
@@ -0,0 +1,21 @@
+package com.company.project.traveloka.ui.profile.adapter
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.company.project.traveloka.ui.profile.fragment.PostFragment
+
+class ProfilesPagerAdapter(activity: AppCompatActivity): FragmentStateAdapter(activity)  {
+
+    override fun getItemCount(): Int = 3
+
+    override fun createFragment(position: Int): Fragment {
+        var fragment: Fragment? = null
+        when (position) {
+            0 -> PostFragment()
+            1 -> PostFragment()
+            2 -> PostFragment()
+        }
+        return fragment as Fragment
+    }
+}
