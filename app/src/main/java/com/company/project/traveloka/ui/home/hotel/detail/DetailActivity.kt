1a518b8cc946ab293b57d293d366206ea8892591
lvinist <alvin.geomatics@gmail.com>
2022-05-25 21:21:42 +0700

    add detailactivity layout

--- app/src/main/java/com/company/project/traveloka/ui/home/hotel/detail/DetailActivity.kt
+++ app/src/main/java/com/company/project/traveloka/ui/home/hotel/detail/DetailActivity.kt
@@ -1,7 +1,9 @@
 package com.company.project.traveloka.ui.home.hotel.detail
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import com.company.project.traveloka.R
 import com.company.project.traveloka.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
@@ -12,5 +14,20 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        getSupportActionBar()?.setDisplayHomeAsUpEnabled(true)
+        getSupportActionBar()?.setDisplayShowHomeEnabled(true)
+
+        title = binding.hotelName.text.toString()
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.details_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
