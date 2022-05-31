9c68106daed79e5af6b2fd549c54e4cc29b4856a
robertheo <57569392+robertheo15@users.noreply.github.com>
2022-05-31 23:35:59 +0700

    Implement HomeViewModel

--- app/src/main/java/com/company/project/traveloka/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/company/project/traveloka/ui/home/HomeViewModel.kt
@@ -1,8 +1,22 @@
 package com.company.project.traveloka.ui.home
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.company.project.traveloka.data.local.model.entitiy.hotel.Hotel
+import com.company.project.traveloka.data.remote.source.repository.hotel.HotelRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor() : ViewModel()
+class HomeViewModel @Inject constructor(
+    private val hotelRepository: HotelRepository
+) : ViewModel() {
+
+    suspend fun findAll(): LiveData<PagingData<Hotel>> {
+        return hotelRepository.findAll().cachedIn(viewModelScope).asLiveData()
+    }
+}
