9853b59a03ba92ca7b4bb34309d5deea4359e5dc
robertheo <57569392+robertheo15@users.noreply.github.com>
2022-05-31 23:34:26 +0700

    Revert "Fix Dummy Data"
    This reverts commit 66a7bc4a00b4fef576c0f3466af2a5428aff6b9a.

--- app/src/main/java/com/company/project/traveloka/utils/constant.kt
+++ app/src/main/java/com/company/project/traveloka/utils/constant.kt
@@ -3,6 +3,4 @@ package com.company.project.traveloka.utils
 object constant {
     const val BASE_URL = "http://127.0.0.1"
     const val LOGIN_DATASTORE = "login_datastore"
-    const val NETWORK_LOAD_SIZE = 20
-    const val HOTEL_API_STARTING_INDEX = 1
 }
