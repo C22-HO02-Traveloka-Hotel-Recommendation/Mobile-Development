f34d115ecd4bf0ee44ce640993e0303f12b15dad
robertheo <57569392+robertheo15@users.noreply.github.com>
2022-06-07 00:35:23 +0700

    Update ReviewApiService.kt

--- app/src/main/java/com/company/project/traveloka/data/remote/source/service/ReviewApiService.kt
+++ app/src/main/java/com/company/project/traveloka/data/remote/source/service/ReviewApiService.kt
@@ -1,3 +1,11 @@
 package com.company.project.traveloka.data.remote.source.service
 
-interface ReviewApiService
+import com.company.project.traveloka.data.local.model.entitiy.review.Review
+import com.company.project.traveloka.data.remote.model.entity.review.ReviewResponse
+import retrofit2.http.GET
+
+interface ReviewApiService {
+
+    @GET("reviews")
+    suspend fun findAll(): ReviewResponse<Review>
+}
