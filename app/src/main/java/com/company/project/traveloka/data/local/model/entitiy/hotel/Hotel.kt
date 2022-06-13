600b1be0d768ff064005faf283e771cf383f0226
robertheo <57569392+robertheo15@users.noreply.github.com>
2022-06-14 00:04:35 +0700

    Update Hotel.kt

--- app/src/main/java/com/company/project/traveloka/data/local/model/entitiy/hotel/Hotel.kt
+++ app/src/main/java/com/company/project/traveloka/data/local/model/entitiy/hotel/Hotel.kt
@@ -1,17 +1,13 @@
 package com.company.project.traveloka.data.local.model.entitiy.hotel
 
 import android.os.Parcelable
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 import kotlinx.parcelize.Parcelize
 
 @JsonClass(generateAdapter = true)
 @Parcelize
-@Entity(tableName = "hotels")
 data class Hotel(
-    @PrimaryKey
     @Json(name = "id")
     val id: String,
 
