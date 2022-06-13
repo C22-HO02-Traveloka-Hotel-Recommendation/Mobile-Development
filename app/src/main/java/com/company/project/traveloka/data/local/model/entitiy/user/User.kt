91cac94e2a1da727ef9c87fcbcda3a86ab43e4f8
robertheo <57569392+robertheo15@users.noreply.github.com>
2022-06-14 00:04:43 +0700

    Update User.kt

--- app/src/main/java/com/company/project/traveloka/data/local/model/entitiy/user/User.kt
+++ app/src/main/java/com/company/project/traveloka/data/local/model/entitiy/user/User.kt
@@ -1,17 +1,13 @@
 package com.company.project.traveloka.data.local.model.entitiy.user
 
 import android.os.Parcelable
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 import kotlinx.parcelize.Parcelize
 
 @JsonClass(generateAdapter = true)
 @Parcelize
-@Entity(tableName = "users")
 data class User(
-    @PrimaryKey
     @Json(name = "id")
     val id: Int?,
 
