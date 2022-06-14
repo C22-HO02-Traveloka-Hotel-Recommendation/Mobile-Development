506243561abd50d84ac56459362ffe30f1191256
robertheo <57569392+robertheo15@users.noreply.github.com>
2022-06-14 20:59:23 +0700

    Refactor code

--- app/src/main/java/com/company/project/traveloka/utils/constant.kt
+++ app/src/main/java/com/company/project/traveloka/utils/constant.kt
@@ -1,6 +1,6 @@
 package com.company.project.traveloka.utils
 
-object constant {
+object Constant {
     const val BASE_URL = "https://traveloka-cloud-run-api-gateway-8q20i5np.ue.gateway.dev/"
     const val LOGIN_DATASTORE = "login_datastore"
     const val NETWORK_LOAD_SIZE = 5
